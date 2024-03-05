902aa08a31f4ba73b446f493f4f7d19aa340ae4a
MoonArchitect <45274732+MoonArchitect@users.noreply.github.com>
2024-03-05 11:29:08 +0700

    fix friendship db table

--- backend/postgresql/migrations/20240303160542_fix_friendship_requests.down.sql
+++ backend/postgresql/migrations/20240303160542_fix_friendship_requests.down.sql
@@ -1,3 +1,8 @@
+ALTER TABLE friendships DROP CONSTRAINT friendships_unique_id_pair;
+ALTER TABLE friendships DROP COLUMN id;
+ALTER TABLE friendships DROP COLUMN created_at;
+ALTER TABLE friendships ADD CONSTRAINT friendships_pkey PRIMARY KEY (user_id);
+
 ALTER TABLE pending_friendship_requests DROP CONSTRAINT pending_friendship_requests_unique_id_pair;
 ALTER TABLE pending_friendship_requests DROP COLUMN id;
 ALTER TABLE pending_friendship_requests ADD CONSTRAINT pending_friendship_requests_pkey PRIMARY KEY (user_id);
--- backend/postgresql/migrations/20240303160542_fix_friendship_requests.up.sql
+++ backend/postgresql/migrations/20240303160542_fix_friendship_requests.up.sql
@@ -1,3 +1,8 @@
 ALTER TABLE pending_friendship_requests DROP CONSTRAINT pending_friendship_requests_pkey;
 ALTER TABLE pending_friendship_requests ADD id BIGSERIAL PRIMARY KEY;
 ALTER TABLE pending_friendship_requests ADD CONSTRAINT pending_friendship_requests_unique_id_pair UNIQUE (user_id, friend_id);
+
+ALTER TABLE friendships DROP CONSTRAINT friendships_pkey;
+ALTER TABLE friendships ADD id BIGSERIAL PRIMARY KEY;
+ALTER TABLE friendships ADD created_at TIMESTAMPTZ NOT NULL DEFAULT NOW();
+ALTER TABLE friendships ADD CONSTRAINT friendships_unique_id_pair UNIQUE (user_id, friend_id);
